d074f07d7700e2af275237c970533494764e6173
Aniket <108017771+ANI2707@users.noreply.github.com>
2024-04-02 09:27:39 +0530

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getMessaging } from "firebase/messaging";
 const firebaseConfig = {
-    apiKey: "AIzaSyByY8Il4u9MOlfiEntXnnToK_h6bAa7ChU",
-    authDomain: "cloud-messaging-49970.firebaseapp.com",
-    projectId: "cloud-messaging-49970",
-    storageBucket: "cloud-messaging-49970.appspot.com",
-    messagingSenderId: "652914102036",
-    appId: "1:652914102036:web:da7ac3c22b3269e9ea7c01"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
 
